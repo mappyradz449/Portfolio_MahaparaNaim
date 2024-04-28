a4f5abe34bbf444bd410765c4cdf5f6bc46f7aa9
Mahapara Naim <mahaparanaim57@gmail.com>
2024-04-28 15:20:36 +0600

    Skills component animate once

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,8 +10,9 @@ const Skill = ({ name, x, y }) => {
          p-8 shadow-dark cursor-pointer"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
-      animate={{ x: x, y: y }}
+      whileInView={{ x: x, y: y }}
       transition={{ duration: 1.5 }}
+      viewport={{ once: true }}
     >
       {name}
     </motion.div>
